e81289b67752fc25e45cdea2b1e6a15c72cc080a
R4Z121 <m.rizkyazizi@gmail.com>
2023-09-24 23:30:01 +0700

    add feature download cv

--- src/components/about/ProfileDescription.jsx
+++ src/components/about/ProfileDescription.jsx
@@ -3,11 +3,18 @@ import ProfileCard from "./ProfileCard";
 import downloadLogo from "../../assets/icons/download.svg";
 import suitcaseLogo from "../../assets/icons/suitcase.svg";
 import medalLogo from "../../assets/icons/medal.svg";
+import cvFile from "../../assets/pdf/cv - r4z121.pdf";
 
 import { getProfileDescription } from "../../utils/data";
+import { useRef } from "react";
 
 export default function ProfileDescription() {
+  const downloadRef = useRef(null);
   const detail = getProfileDescription();
+  const downloadCvHandler = (ref) => {
+    ref.current.click();
+  }
+
   return (
     <div className="flex flex-col gap-6 items-center md:items-start max-w-96">
       <div className="flex flex-col gap-6 items-center md:items-start">
@@ -18,7 +25,8 @@ export default function ProfileDescription() {
         <ProfileCard imageSource={medalLogo} title="Experiences" description={`${detail.experience}+ years`} />
         <ProfileCard imageSource={suitcaseLogo} title="Completed" description={`${detail.completeProjects}+ projects`} />
       </div>
-      <LinkedButton img={downloadLogo} altImg="download-icons" content="Download CV" />
+      <LinkedButton img={downloadLogo} altImg="download-icons" content="Download CV" clickEvent={() => downloadCvHandler(downloadRef)} />
+      <a href={cvFile} className="hidden" ref={downloadRef} download></a>
     </div>
   )
 }
--- src/components/navbar/NavList.jsx
+++ src/components/navbar/NavList.jsx
@@ -1,9 +1,9 @@
 import NavItems from "./NavItems"
 
-export default function NavList({show,navRefs,linkedRefHandler}) {
+export default function NavList({show, scrolledNav, navRefs,linkedRefHandler}) {
   return (
     <div className="relative">
-      <ul className={`w-full flex flex-col bg-app-red absolute ${show ? 'translate-y-0' : '-translate-y-full'} transition-transform sm:transition-none sm:relative sm:translate-y-0 sm:flex-row sm:rounded-bl-xl sm:overflow-hidden z-10`}>
+      <ul className={`w-full flex flex-col bg-app-red absolute ${show ? 'translate-y-0' : '-translate-y-full'} transition-transform sm:transition-none sm:relative sm:translate-y-0 sm:flex-row ${scrolledNav ? 'sm:rounded-bl-0' : 'sm:rounded-bl-xl'} sm:overflow-hidden z-10`}>
         <NavItems content="Home" refLink={navRefs.jumboRef} linkedRefHandler={linkedRefHandler} />
         <NavItems content="About" refLink={navRefs.aboutRef} linkedRefHandler={linkedRefHandler} />
         <NavItems content="Skills" refLink={navRefs.skillRef} linkedRefHandler={linkedRefHandler} />
